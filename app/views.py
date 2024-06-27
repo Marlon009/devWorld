13c27d847aa5cd1715f45cee935e27f077ae3af7
iago correia de lima <115280746+iagob2@users.noreply.github.com>
2024-06-26 21:16:20 -0300

    base

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+from django.template import loader
+
+# Create your views here.
+
+def appHome(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render())
